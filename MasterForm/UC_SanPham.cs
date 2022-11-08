0de01e98fe9e080831618364fa4a64e76e4743e0
Tran Trong Nhan <6151071016@st.utc2.edu.vn>
2022-11-08 11:16:53 +0700

    commit 8/11

--- MasterForm/UC_SanPham.cs
+++ MasterForm/UC_SanPham.cs
@@ -14,6 +14,8 @@ namespace MasterForm
 {
     public partial class UC_SanPham : UserControl
     {
+         private string nameHost = ConfigurationManager.AppSettings.Get("nameHost");
+        private string userNamePss = ConfigurationManager.AppSettings.Get("userName");
         List<SanPham> SanPhams = new List<SanPham>();
  
         List<string> DanhMucs = new List<string>();
@@ -23,8 +25,8 @@ namespace MasterForm
         {
             String connectionstring;
             SqlConnection cnn;
-            connectionstring = @"Data Source = LAPTOP-AAL40GA9\SQLEXPRESS ;Initial Catalog=BanHang;
-            User ID=sa; Password=demo123";
+            connectionstring = @"Data Source = " + nameHost + " ;Initial Catalog=BanHang;"
+            + userNamePss + ";";
             cnn = new SqlConnection(connectionstring);
             cnn.Open();
             SqlCommand command;
